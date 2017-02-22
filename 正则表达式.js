1d1863c75b5a3a2395fd26da4547f602c1b8ebcd
linshuang <linshuang@roadclouding.com>
2017-02-22 19:30:20 +0800

    commit  js

--- 正则表达式.js
+++ 正则表达式.js
@@ -48,7 +48,7 @@ var re5 = /\d/g;  //global全球的
 console.log(str4.match(re4));// [ '4', index: 5, input: 'sdf e4535 fdfda 454 454rtt 7' ]
 console.log(str4.match(re5));// [ '4', '5', '3', '5', '4', '5', '4', '4', '5', '4', '7' ]
 
-var re6 = /\d+/g; // + 表示/d 数字是任意长度
+var re6 = /\d+/g; //    + 表示/d 数字是任意长度
 console.log(str4.match(re6)); //[ '4535', '454', '454', '7' ]
 
 /* eg 5 */
@@ -57,6 +57,48 @@ var str5 = 'abcadef';
 
 console.log(str5.replace('a','T')); // Tbcadef
 console.log(str5.replace(/a/,'T')); // Tbcadef
-console.log(str5.replace(/a/g,'T')); // TbcTdef
+console.log(str5.replace(/a/g,'T')); // TbcTde
+console.log(str5.replace(/a|c/g,'E')); //EbEEdef | 的意思就是“和”
 
-fgf 
+// eg 6
+
+var str6 = '1a2 abc ee 1c2';
+var re7 = /1[abc]2/g; // [] : 的意思 1a2或者1b2或者1c2
+                      //  [0-9] :0到9 [a-z]:a到z的所有字母
+                        // [0-9a-z] :
+                        // [^a] : 除了a以外的所有的东西
+                        // [^0-9a-zA-Z] : 除了0到9字母以外的所有的
+console.log(str6.match(re7)); //[ '1a2', '1c2' ]
+
+//eg 7
+
+var str7 = '<dfdfaaffdsft554t.....>fdafa'
+var re8 = /<.+>/g   //. 表示所有的东西,任意字符(英文，数字等)
+console.log(str7.replace(re8,'***')); // ***fdafa
+
+/*
+    \d digital
+    \D [^0-9] 非数字
+
+    \w word [a-z0-9_] 包括数字、字母、下划线
+    \W [^a-z0-9_] 除了数字 字母 下划线 以外的所有
+
+    \s space 空白
+    \S 除了空白以外的
+      { n , m } 最少 n次，最多 m 次。
+      { n ,  } 最少 n次，最多 不限。
+      {   , m } 最少 不限，最多 m 次。
+      { n }  正好n次
+
+
+ */
+
+//eg 8
+var str8 = '  fdfa  fd afa fdafaa ffrgfg ';
+console.log(str8.replace(/\s+/g,'*'));//*fdfa*fd*afa*fdafaa*ffrgfg*
+
+//eg 9
+var str9 = '我的qq是：111122336 , a16你的是 1234567？' ;
+var re = /[1-9]\d{4,10}/g ;
+
+console.log(str9.match(re)); //[ '111122336', '1234567' ]
