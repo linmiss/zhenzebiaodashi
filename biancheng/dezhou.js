8867ef9f4500f42bfb40b0e6a4e28bc815cb88b2
Linmiss <linmiss8128@gmail.com>
2017-03-19 21:15:26 +0800

    doing it

--- biancheng/dezhou.js
+++ biancheng/dezhou.js
@@ -1,6 +1,6 @@
 "use strict"
 
-//皇家同花顺
+//皇家同花顺:最大牌：A-K-Q-J-10
 var huangJiaTHS = (num, color) => {
     if (num == [10, 11, 12, 13, 14].toString() && color == ["s", "h", "d", "c"].toString()) {
         return true;
@@ -11,25 +11,72 @@ var huangJiaTHS = (num, color) => {
 };
 
 //console.log(huangJiaTHS([10, 11, 12, 13, 14], ["s", "h", "d", "c"]));
-//同花顺
+//同花顺:最大牌：K-Q-J-10-9 最小牌：A-2-3-4-5
 var tongHuaS = (num, color) => {
     for (let i = 1; i < num.length; i++) {
-        if (num[i] == num[i - 1] + 1) {
-            return num;
+        if ((num[i] == num[i - 1] + 1 || num == [2, 3, 4, 5, 14]) && color == ["s", "h", "d", "c"].toString()) {
+            return true;
         } else {
-            num = null;
+            return false;
         }
     }
-    return num;
 };
 
+//console.log(tongHuaS([2, 3, 4, 5, 14],["s", "h", "d", "c"]))
+
+//四条:最大牌：A-A-A-A-K 最小牌：2-2-2-2-3
+var siTiao = (num, color) => {
+    for (let i = 0; i < num.length - 2; i++) {
+        if ((num[i] == num[i + 1] && num[3] != num[4]) ||
+            ((num[i + 1] == num[i + 2] && num[3] == num[4]) && num[0] != num[1])) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+console.log(siTiao([3, 3, 3, 3, 10]));
+
+
+//葫芦：最大牌：A-A-A-K-K 最小牌：2-2-2-3-3
+var huLu = (num, color) => {
+    for (var i = 0; i < 2; i++) {
+        if (num[i] == num[i + 1]) {
+
+        }
+    }
+};
+
+//同花：同一花色。（最大牌：A-K-Q-J-9 最小牌：2-3-4-5-7）
+
+//顺子：花色不一样的顺子。（最大牌：A-K-Q-J-10 最小牌：A-2-3-4-5
+
+//三条：三同张加两单张。（最大牌：A-A-A-K-Q 最小牌：2-2-2-3-4
+
+//两对：（最大牌：A-A-K-K-Q 最小牌：2-2-3-3-4
+
+//一对：（最大牌：A-A-K-Q-J 最小牌：2-2-3-4-5
+
+//单牌：（最大牌：A-K-Q-J-9 最小牌：2-3-4-5-7）
+
+/*
+牌型大小：   
+1、皇家同花顺＞同花顺＞四条＞葫芦＞同花＞顺子＞三条＞两队＞一对＞单牌 
+
+2、牌点从大到小为：A、K、Q、J、10、9、8、7、6、5、4、3、2，各花色不分大小。   
+
+3、同种牌型，对子时比对子的大小，其它牌型比最大的牌张，如最大牌张相同则比第二大的牌张，
+以此类推，都相同时为相同
+
+*/
 
 
 $.getJSON("./match.json", function(data, status) {
 
     const re = /[0-9A-Z]+/g;
     const re1 = /[a-z]/g;
-    console.log(data.matches.length);
+    //console.log(data.matches.length);
     for (let i = 0; i < data.matches.length; i++) {
         var alice = data.matches[i].alice,
             bob = data.matches[i].bob;
