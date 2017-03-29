0ce99b0275467a1e230a8ee96a3887773fd0887f
linshuang <linshuang@roadclouding.com>
2017-03-29 10:36:48 +0800

    ..

--- biancheng/dezhou.js
+++ biancheng/dezhou.js
@@ -1,5 +1,4 @@
-"use strict"
-
+'use strict'
 //皇家同花顺
 var huangJiaTHS = (num, color) => {
     if (num == [10, 11, 12, 13, 14].toString() && color == ["s", "h", "d", "c"].toString()) {
@@ -29,25 +28,43 @@ $.getJSON("./match.json", function(data, status) {
 
     const re = /[0-9A-Z]+/g;
     const re1 = /[a-z]/g;
-    console.log(data.matches.length);
-    for (let i = 0; i < data.matches.length; i++) {
-        var alice = data.matches[i].alice,
-            bob = data.matches[i].bob;
-        var num1 = alice.match(re); //面值
-        var color1 = alice.match(re1); //花色
 
-        /*  10 = T ; 11 = J ; 12 = Q ; 13 = K ;  14 = A  */
-        var num1 = num1.toString().replace(/T/g, '10');
-        var num1 = num1.toString().replace(/J/g, '11');
-        var num1 = num1.toString().replace(/Q/g, '12');
-        var num1 = num1.toString().replace(/K/g, '13');
-        var num1 = num1.toString().replace(/A/g, '14');
-        //console.log(num);
-        var num1 = num1.split(",").sort((a, b) => a - b);
-        var color1 = color1.sort((a, b) => b - a);
-        //console.log(num1);
-        //console.log(color1);
-        //console.log("-----------");
-    };
+    /* 牌型处理函数 */
+    function deal(data) {
+
+        for(let i = 0; i < data.matches.length; i++) {
+
+            // var num = data.matches[i].alice.match(re);
+            // console.log(num);
+            console.log(data.length)
+        }
+
+    }
+
+    deal();
+
+
+    //console.log(data.matches.length);
+    // for (let i = 0; i < data.matches.length; i++) {
+    //
+    //     /* alice */
+    //     var alice = data.matches[i].alice,
+    //         bob = data.matches[i].bob;
+    //     var num1 = alice.match(re); //面值
+    //     var color1 = alice.match(re1); //花色
+    //
+    //     /*  10 = T ; 11 = J ; 12 = Q ; 13 = K ;  14 = A  */
+    //     var num1 = num1.toString().replace(/T/g, '10');
+    //     var num1 = num1.toString().replace(/J/g, '11');
+    //     var num1 = num1.toString().replace(/Q/g, '12');
+    //     var num1 = num1.toString().replace(/K/g, '13');
+    //     var num1 = num1.toString().replace(/A/g, '14');
+    //     //console.log(num);
+    //     var num1 = num1.split(",").sort((a, b) => a - b);
+    //     var color1 = color1.sort((a, b) => b - a);
+    //     console.log(num1);
+    //     //console.log(color1);
+    //     //console.log("-----------");
+    // };
 
 });
